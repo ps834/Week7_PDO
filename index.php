2ee54a856c8939d19eeeb207f5ebdd260ec6f6ca
Pooja Suvarna <ps834@afsconnect2.njit.edu>
2017-11-02 22:47:31 -0400

    Add Abstract HTML function

--- index.php
+++ index.php
@@ -15,49 +15,92 @@
 		try{				
 				$conn = new PDO("mysql:host=$hostname;dbname=ps834",$username,$pwd);
 				echo "Connected successfully ";
-				
-				$obj1 = new runSelect();
-				$obj1->selectQuery($conn);
+				$condition = "where id < 6";
+				$obj1 = new runQueries();
+				$obj1->selectQuery($conn,$condition);
 
 			}catch(PDOException $e){
 
 				echo "Error";
 			}
-
 		}
 
-
+		
 	}
 
 
 
 
-	class runSelect{
+	class runQueries
+	{
 
 
-		 public function selectQuery($conn){
-		 	echo "inside select";		 	
-			$sqlQuery = "select * from accounts";
-			$results = $this::runQuery($sqlQuery,$conn);
-			print_r($results);
-		}
+		static function runQuery($query,$conn) {
 
-
-		public function runQuery($query,$conn) {
 
 		    try {
+					$q = $conn->prepare($query);
+					$q->execute();
+					$results = $q->fetchAll();
+					echo "fetch!!";
+					$q->closeCursor(); 
+					return $results;	
 
-				$q = $conn->prepare($query);
-				$q->execute();
-				$results = $q->fetchAll();
-				$q->closeCursor(); 
-				return $results;	
 			} catch (PDOException $e) {
 				http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage());
 			}	  
 		}
 
+
+		 public function selectQuery($conn,$condition){
+
+			$sqlQuery = "select * from accounts $condition";
+			$results = runQueries::runQuery($sqlQuery,$conn);
+			print_r($results);
+			$display = new displayResults();
+			$display->displayTable();
+
+		}
+
+
 	}
+
+
+	class displayResults{
+
+		function displayTable($results){
+
+			$this->html .= '<tr>';
+			foreach($results as $rows){
+				foreach($rows as $values){
+
+				}
+			} 
+
+
+
+		}
+
+
+	}
+
+
+	abtract class html{
+
+		protected $html;
+
+		public function __construct(){
+
+			$this->html = '<html><body><table>';
+		}
+
+		public function __destruct(){
+
+			$this->html .= '</table></body></html>';
+		}
+
+	}
+
 
 
 
